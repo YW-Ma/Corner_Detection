65d3848ef3133fee6dd49988ef33e5e90166ad99
WHU-Traffic-Sign-Detection-Team <Ma_Yaowei@outlook.com>
2019-03-09 11:35:33 +0800

    Build the structure, add OpenCV's tutorial

---
+++ Harris_Corner_Detector.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as image
+
+def cornerFinder_Harris(img,win_size=5,sigma=0.4,k=0.04):
+    """
+    Parameters:
+        img: input gray image, grayscale(0-255)
+        win_size: sliding window size
+        sigma: parameter of Gauss filtering model, set 0 to turn the model off
+        k: a constant in Harris corner detection. OpenCV tutorial set it to 0.04
+    Return:
+        A grayscal image(float32) with value from 0 to 255 which marks all the corners detected.
+    """
+    corners = img
+    return corners
+
+pathName = r'Image\chess.bmp'
+img = cv2.imread(pathName) #dtype = uint8
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) #dtype = uint8
+gray = np.float32(gray) #dtype uint8 --> float32 
+
+corners = cornerFinder_Harris(gray,5,0.4,0.04)
+
+img[dst>0.01*corners.max()]=[0,0,255] #mark the result
+cv2.imshow('dst',img)
+if cv2.waitKey(0) & 0xff == 27:
+    cv2.destroyAllWindows()
