3d1c8fa1e22eb6a3517d4b07e3926345b8d72171
WHU-Traffic-Sign-Detection-Team <Ma_Yaowei@outlook.com>
2019-03-09 12:03:00 +0800

    GaussianBlur

--- Harris_Corner_Detector.py
+++ Harris_Corner_Detector.py
@@ -3,16 +3,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as image
 
-def cornerFinder_Harris(img,win_size=5,sigma=0.4,k=0.04):
+def cornerFinder_Harris(img,win_size=5,sigmaXY=0.4,k=0.04):
     """
     Parameters:
         img: input gray image, grayscale(0-255)
         win_size: sliding window size
-        sigma: parameter of Gauss filtering model, set 0 to turn the model off
+        sigmaXY: parameter of GaussianBlur model, set 0 to turn the model off; Both sigmaX and sigmaY are specified by sigmaXY.
         k: a constant in Harris corner detection. OpenCV tutorial set it to 0.04
     Return:
         A grayscale image(float32) with value from 0 to 255 which marks all the corners detected.
     """
+    #1. Get the gradiant of the whole image
+    [dx,dy] = np.gradient(img)
+    #2. GaussianBlur [optional]
+    if(sigmaXY!=0):
+        dx=cv2.GaussianBlur(dx,(win_size,win_size),sigmaXY,sigmaXY)
+        dy=cv2.GaussianBlur(dy,(win_size,win_size),sigmaXY,sigmaXY) 
+    #3. Calculate Intensit. I = det(M)-k*tr(M)
+
+    
+    cv2.imshow('dx',dx)
+    cv2.imshow('dy',dy)
     corners = img
     return corners
 
